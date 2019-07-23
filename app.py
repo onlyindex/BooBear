26fb625fe21f80a4c42fc99f5d28b2aea7300bd7
index <index@indexdeMac-mini.local>
2019-07-23 11:48:19 +0800

    尝试一下网站地图组织碎片化信息

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask,render_template
 from datetime import timedelta
 
 app = Flask(__name__)
+# 设置为开发模式
+app.config['DEBUG'] = True
 
 # 设置缓存时间1s
-app.config['SEND_FILE_MAX_AGE_DEFAULT '] = timedelta(seconds=1)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = timedelta(seconds=1)
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template('home.html')
 
